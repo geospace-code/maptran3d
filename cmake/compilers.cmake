32e7aad02316a9a439f1c6d9e624ed898edbc941
Michael <10931741+scivision@users.noreply.github.com>
2022-05-16 12:01:54 -0400

    remove unneeded flags

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,12 +1,12 @@
 if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
   add_compile_options(
-  -warn -traceback -heap-arrays
-  $<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>
+  -warn -traceback
   "$<$<CONFIG:Debug,RelWithDebInfo>:-check>"
   "$<IF:$<EQUAL:${realbits},32>,,-r8>"
   )
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  add_compile_options(-mtune=native -Wall -fimplicit-none
+  add_compile_options(
+  -Wall -fimplicit-none
   "$<$<CONFIG:Debug,RelWithDebInfo>:-fcheck=all;-Werror=array-bounds>"
   "$<IF:$<EQUAL:${realbits},32>,,-fdefault-real-8>"
   )
