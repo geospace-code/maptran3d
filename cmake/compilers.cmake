0edb2b16af77b18c85e47db7e57e16757b9b9c32
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-08 15:26:24 -0400

    cmake: config proper install filename

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -4,12 +4,12 @@ if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
   "$<$<CONFIG:Debug,RelWithDebInfo>:-check>"
   "$<IF:$<EQUAL:${realbits},32>,,-r8>"
   )
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   add_compile_options(
   -Wall -fimplicit-none
   "$<$<CONFIG:Debug,RelWithDebInfo>:-fcheck=all;-Werror=array-bounds>"
   "$<IF:$<EQUAL:${realbits},32>,,-fdefault-real-8>"
   )
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL NAG)
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "NAG")
   add_compile_options(-f2018 -C -colour -gline -nan -info -u)
 endif()
